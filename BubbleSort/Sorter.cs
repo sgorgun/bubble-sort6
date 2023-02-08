98c0774cb2c4aca6d308c5a13af571238f8aac35
@gorgun.sergei <gorgun.sergei@student.ehu.lt>
2023-02-08 16:55:16 +0200

    Ready

--- BubbleSort/Sorter.cs
+++ BubbleSort/Sorter.cs
@@ -10,8 +10,31 @@ namespace BubbleSort
         /// </summary>
         public static void BubbleSort(this int[]? array)
         {
-            // TODO #1. Implement the method using a loop statements.
-            throw new NotImplementedException();
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array), $"{nameof(array)} is null");
+            }
+
+            if (array == Array.Empty<int>())
+            {
+                throw new ArgumentException($"{nameof(array)} is empty.", nameof(array));
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(ref array[j], ref array[j + 1]);
+                    }
+                }
+            }
+        }
+
+        public static void Swap(ref int a, ref int b)
+        {
+            (a, b) = (b, a);
         }
 
         /// <summary>
@@ -19,8 +42,44 @@ namespace BubbleSort
         /// </summary>
         public static void RecursiveBubbleSort(this int[]? array)
         {
-            // TODO #2. Implement the method using recursion algorithm.
-            throw new NotImplementedException();
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array), $"{nameof(array)} is null");
+            }
+
+            if (array == Array.Empty<int>())
+            {
+                throw new ArgumentException($"{nameof(array)} is empty.", nameof(array));
+            }
+
+            BubbleSort(array, array.Length);
+        }
+
+        private static void BubbleSort(int[] array, int n)
+        {
+            if (n == 1)
+            {
+                return;
+            }
+
+            int count = 0;
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (array[i] <= array[i + 1])
+                {
+                    continue;
+                }
+
+                Swap(ref array[i], ref array[i + 1]);
+                count++;
+            }
+
+            if (count == 0)
+            {
+                return;
+            }
+
+            BubbleSort(array, n - 1);
         }
     }
 }
